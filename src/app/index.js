777fdf3b1815e7dbdb75a34718c5f2a44efcc9cc
air mac <sarnor@mail.ru>
2025-03-09 09:42:47 +0400

    ff

--- src/app/index.js
+++ src/app/index.js
@@ -4,3 +4,12 @@ import '../style/index.less';
 import htmlPages from './import/index.js';
 
 import httpAjax from './ajax/index.js'
+
+const h1 = document.querySelector('h1')
+console.log(h1);
+
+function foot(data = 'default') {
+    console.log(data);
+}
+
+h1.addEventListener('click', foot)
